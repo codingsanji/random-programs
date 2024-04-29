ddd937ef640bcfa988038ec01b2a1e4823e2bdcf
Deydeepya <165115144+codingsanji@users.noreply.github.com>
2024-04-29 11:52:28 +0300

    changes to previous code

--- Java/printArray.java
+++ Java/printArray.java
@@ -10,7 +10,18 @@ public class printArray {
     }
     public void arrayDemo() {
         int[] myArray=new int[5];
+        myArray[1] = 2; 
+        myArray[2] = 3;
+        myArray[3] = 4;
+        myArray[4] = 5;
+        myArray[0] = 1;
+        myArray[4] = 9; //UPDATED ELEMENT 4 FROM 5 TO 9
         printArray(myArray);
+        System.out.println("Number of elements: " + myArray.length);
+        System.out.println("Last Element in Array: " + myArray[myArray.length-1]);
+        System.out.println("Last Index in Array: " + (myArray.length-1));
+        //int LastIndex = myArray.length-1;
+        //System.out.println("Last Index in Array: " + LastIndex);
     }
      public static void main(String[] args) {
     	 printArray Array1 = new printArray();
