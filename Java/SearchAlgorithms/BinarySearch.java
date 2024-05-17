1828d913d1494a5694f8f516f6c85521bfc3d6d5
Deydeepya <ddpyawork@gmail.com>
2024-05-17 20:07:44 +0300

    Create BinarySearch.java

---
+++ Java/SearchAlgorithms/BinarySearch.java
@@ -0,0 +1,49 @@
+package Java;
+import java.util.*;
+
+public class BinarySearch {
+	public static int binarySearch(int[] arr , int target) {
+		int min=0;
+		int max=arr.length-1;
+		while (min<=max) {
+			int mid =(min+max)/2;
+			if (arr[mid]>target) {
+				max=mid-1;
+			} else if(arr[mid]<target) {
+				max=mid+1;
+			} else if (arr[min]==target) {
+				return mid;
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		//user inputs their array
+		System.out.print("How many values does your array have: ");
+		int count=scanner.nextInt();
+		scanner.nextLine();
+		int[] yourarr = new int[count];
+		for (int i=0 ; i<count ; i++) {
+			System.out.println("Value "+ (i+1) + ": ");
+			int value=scanner.nextInt();
+			yourarr[i]=value;
+		}
+		scanner.nextLine();
+		
+		//gets the target value
+		System.out.print("Your target value: ");
+		int target=scanner.nextInt();
+		
+		int index=binarySearch(yourarr , target);
+		if (index!=-1) {
+			System.out.println("The index of "+ target + " is "+ index + ".");
+		} else {
+			System.out.println("Your target isn't there in your inputted array.");
+		}
+		
+		scanner.close();
+	}
+}
