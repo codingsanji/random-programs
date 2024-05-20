cc28cdd2bf8b3123446914d7df8407a7f3e61ace
Deydeepya <ddpyawork@gmail.com>
2024-05-20 10:30:16 +0300

    Create SqrtCheck.java

---
+++ Java/SqrtCheck.java
@@ -0,0 +1,21 @@
+package Java;
+import java.util.*;
+
+public class SqrtCheck {
+	public static void main(String[] args){
+        Scanner scanner  = new Scanner(System.in);
+        System.out.print("Enter the value you want to check if it is a squared value: ");
+        int value = scanner.nextInt();
+        
+        //to check if value is a squared number you must square root it
+        double check=Math.sqrt(value);
+        
+        if (check == (int) check) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
+
+
